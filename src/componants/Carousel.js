1e3f271f5803a0c81a382c7f5b48e36dd386c8a7
Evan St Martin <shellystmartin@evan.local>
2022-10-19 18:20:44 -0500

    fixed hosting issue

--- src/componants/Carousel.js
+++ src/componants/Carousel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Carousel from 'nuka-carousel';
-import { easeCircleOut, easeElasticOut } from 'd3-ease';
+
 import "../styles/Carousel.css";
 import Acupuncture from '../slider-images/acupuncture-pain-relief.jpeg';
 import Canoe from '../slider-images/canoe.jpeg';
@@ -8,11 +8,11 @@ import Backcrack from '../slider-images/chiro.jpg';
 import Medicine from '../slider-images/functional-medicine.jpg';
 
 
-export default class extends React.Component {
+export default class Slider extends React.Component {
     render() {
         return (
             <div className="silder-component">
-                <Carousel easing={easeCircleOut} edgeEasing={easeElasticOut} wrapAround
+                <Carousel
                     heightMode="max"
                     animation='zoom'
                     autoplay='true'
