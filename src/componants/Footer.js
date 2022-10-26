fc3442495c9f78530c5443463f63407d5c7d7ff7
Evan St Martin <shellystmartin@evan.local>
2022-10-26 15:31:58 -0500

    changed basename so routes work

--- src/componants/Footer.js
+++ src/componants/Footer.js
@@ -8,7 +8,7 @@ const FooterComponant = () => {
   
             
                 <p className="about-website">
-                    All Rights Reserved | Website by <a href="https://webdevev.com/" target="_blank" rel="noreferrer">WebDevEv</a> |
+                    All Rights Reserved | Website by <a href="https://webdevev.herokuapp.com/" target="_blank" rel="noreferrer">WebDevEv</a> |
 
                     {/* updates year for me */}
                     {new Date().getFullYear()}&nbsp;
