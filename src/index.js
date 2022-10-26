c6b38035b16633ffb6332d28533a7be9b9a737a1
Evan St Martin <shellystmartin@evan.local>
2022-10-26 11:46:49 -0500

    removed bootsrap import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "../../node_modules/bootstrap/dist/css/bootstrap.css"
+// import "../../node_modules/bootstrap/dist/css/bootstrap.css"
 import  {BrowserRouter, Routes, Route}  from "react-router-dom";
 import AboutUs from './Pages/AboutTanae';
 import Contactform from './Pages/Contactform';
