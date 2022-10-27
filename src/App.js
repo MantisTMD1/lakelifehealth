b13aa7bc47a19101baa4d45443e6a6552cdf2fb8
Evan St Martin <shellystmartin@evan.local>
2022-10-27 09:26:10 -0500

    refactored index.js/ app.js files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from "react-dom/client";
 // companonants on my home page
 import NavbarComponant from "./componants/Navbar";
 import HeaderComponant from "./componants/Header";
@@ -8,8 +9,11 @@ import SliderComponent from "./componants/Carousel";
 import ScheduleComponant from "./componants/Schedule";
 import LocationComponant from "./componants/Location";
 import FooterComponant from "./componants/Footer";
-
-
+// Routes
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutUs from "./Pages/AboutTanae";
+import Contactform from "./Pages/Contactform";
+import Services from "./Pages/Services";
 
 
 const App = () => {
@@ -17,15 +21,33 @@ const App = () => {
     <div className="App">
       <NavbarComponant />
       <HeaderComponant />
-      <SliderComponent/>
+      <SliderComponent />
       <ScheduleComponant />
       <LocationComponant />
       <FooterComponant />
 
     </div>
 
-
   );
+
+
+
+
 }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter forceRefresh={true}>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="AboutUs" element={<AboutUs />} />
+      <Route path="Contactform" element={<Contactform />} />
+      <Route path="Services" element={<Services />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 // import "../../node_modules/bootstrap/dist/css/bootstrap.css"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AboutUs from "./Pages/AboutTanae";
-import Contactform from "./Pages/Contactform";
-import Services from "./Pages/Services";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter forceRefresh={true}>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="AboutUs" element={<AboutUs />} />
-      <Route path="Contactform" element={<Contactform />} />
-      <Route path="Services" element={<Services />} />
-    </Routes>
-  </BrowserRouter>
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+    </BrowserRouter>,
+  document.getElementById('root')
 );
